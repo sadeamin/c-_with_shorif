d67b652733811cb9a381b6be88e91accd2bf838d
TechTouhid <touhidalamin123@gmail.com>
2022-06-11 12:59:41 +0600

    first commit

---
+++ diff_c_cpp.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main()
+{
+    // int a;
+    // a = 5;
+    
+    // int b;
+
+    // for(int i=1; i<=5; i++){
+    //     int b = i+ 1;
+    //     cout<<b<<endl;
+    // }
+    
+
+    cout<<"alamin"<<endl;
+
+    return 0;
+}
